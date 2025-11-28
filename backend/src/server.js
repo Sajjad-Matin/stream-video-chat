7ee4a6dc9e0be6f5561ba67c5e6f585873d8f8bc
Sajjad Matin <sajjadmatinm@gmail.com>
2025-11-28 17:04:56 +0430

    Change

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,12 +16,10 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://stream-video-chat.vercel.app'
-  ],
+  origin: 'https://stream-video-chat.vercel.app',
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser())
 
