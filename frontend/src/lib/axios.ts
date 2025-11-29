61b9b8e0ab6aba7a40c3d29caaabf26159833f83
Sajjad Matin <sajjadmatinm@gmail.com>
2025-11-29 23:08:55 +0430

    feat: Configure Axios to send JSON headers and enable URL-encoded body parsing in Express.

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,7 +4,11 @@ const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:2002
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
 })
 
 export async function getUserFriends() {
