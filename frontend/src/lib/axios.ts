b11d711bc6ea6a85bb445d189a1b0f52e34e83ef
Sajjad Matin <sajjadmatinm@gmail.com>
2025-11-29 22:29:40 +0430

    fix: Restrict CORS origin to only the Vercel application.

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:2002/api" : "https://stream-video-chat-backend.onrender.com";
+const BASE_URL = import.meta.env.MODE === 'development' ? "http://localhost:2002/api" : "https://stream-video-chat-backend.onrender.com/api";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
