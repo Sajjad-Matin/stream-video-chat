181e496cd8871433c2f545bc8c1d9b751d0d63f1
Sajjad Matin <sajjadmatinm@gmail.com>
2025-11-30 20:06:05 +0430

    Configure JWT cookie to always be secure and allow cross-site requests.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -111,8 +111,8 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
 
     res.status(201).json({ success: true, user });
